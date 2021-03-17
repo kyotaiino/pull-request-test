2745ca3218fe9d8d653c631b5109f93550943b65
Kyota Iino <k.iino@outlook.com>
2021-03-17 14:24:56 +0900

    Add sort

--- sort.js
+++ sort.js
@@ -1,2 +1,12 @@
+function sort(num) {
+   number.sort(function (a, b) {
+       if (a == b) {
+           return 0;
+       }
+       return a < b ? -1 : 1;
+   });
+}
+
 var number = [19, 3, 81, 1, 24, 21];
+sort(number);
 console.log(number);
